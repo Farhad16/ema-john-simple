a794fb744511111ff18c5059f00585857d828866
Farhad16 <fhossain389@gmail.com>
2020-08-24 15:26:42 +0600

    Some change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
 
 function App() {
+
   return (
     <div>
       <Header></Header>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from '../../images/logo.png'
 import './Header.css'
-const Header = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Header = (props) => {
+    const cart = props.cart;
+    console.log(cart);
+    // console.log(totalCart.length);
     return (
         <div className="header">
             <img src={logo} alt="" />
-            <nav>
+            <nav className="navber">
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
                 <a href="/manage">Manage Inventory</a>
+                <span className="cart"><FontAwesomeIcon icon={faShoppingCart} /></span>
             </nav>
         </div>
     );
